43e7e0934673fe98e150e8bea8393d682e7dcc57
Lavmarina <go.lavmarina@gmail.com>
2023-03-28 14:59:19 +1000

    first commit

---
+++ getPercents.test.js
@@ -0,0 +1,24 @@
+import getPercents from './task/task_01';
+
+describe("test function getPercents",
+ () =>
+  it('getPercent 20% from 500', () => {
+    const result = getPercents(20,500);
+    expect(result).toBe(100);
+  }),
+  it('getPercent 15% from 60', () => {
+    const result = getPercents(15,60);
+    expect(result).toBe(9);
+  })
+   );
+ 
+ 
+ 
+ 
+
+ 
+ 
+
+
+
+
---
+++ task/task_01.js
@@ -0,0 +1,4 @@
+function getPercents(percent, number) {
+  return ( percent * number)/100;
+}
+export default getPercents;
